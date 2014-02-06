6763306ef754e38f3346915d9780b986b65ec64c
joe kaiser <joe.kaiser@parkwhiz.com>
2014-02-06 14:40:09 -0600

    require_once

--- backend/db_obj.php
+++ backend/db_obj.php
@@ -5,36 +5,32 @@ $db = new Mysqlidb('localhost', 'booking', 'booking', 'booking');
 
 //insert
 
-/*$insertData = array(
-	'start' => time(),
-	'end' => time(),
-	'name' => "John Doe"
-);
+function write($start, $end, $name, $id=null) {
+	$data = array(
+		'start' => $start,
+		'end' => $end,
+		'name' => $name
+	);
 
-if($db->insert('booking', $insertData)) echo 'success!';*/
+	if($id) {
+		$this->db->where('id', $id);
+		$this->db->update('booking', $data);
+	} elseif($this->db->insert('booking', $data)) return true;
 
-//select
+	return false;
+}
 
-/*$results = $db->get('booking', 100);
-print_r($results);*/
+function select($limit=0, $start=null, $end=null) {
+	if($start && $end) {
+		$this->db->where('start', $start);
+		$this->db->where('end', $end);
+	}
+	$results = $this->db->get('booking', $limit);
+	return $results;
+}
 
-//select from time range
-
-/*
-$db->where('start', time());
-$db->where('end', time());
-$results = $db->get('booking');
-print_r($results);*/
-
-//update
-/*$updateData = array(
-	'start' => time(),
-	'end' => time(),
-	'name' => "John Doe"
-);
-$db->where('id', 1);
-$results = $db->update('booking', $updateData);*/
-
-//delete
-/*$db->where('id', 1);
-if($db->delete('booking')) echo 'successfully deleted'; */
+function delete($id) {
+	$this->db->where('id', 1);
+	if($this->db->delete('booking')) return true;
+	return false;
+}
