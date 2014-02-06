0ab213c5d02452e58d4c5f607867ab5d3b57f3cc
Eric Stein <toba@des.truct.org>
2014-02-06 13:48:40 -0600

    api handler

--- backend/backend.php
+++ backend/backend.php
@@ -4,4 +4,28 @@ $adminpw = "hunter2";
 
 echo "hello world";
 
+
+function handle_api() {
+	$csrfcookie = $_COOKIE["csrftoken"];
+	
+	$data = false;
+	$error = null;
+
+
+	# dispatch here
+
+	$data = 'hi';
+
+
+	$response = array(
+		"data" => $data;
+	);
+
+	if ($error) {
+		$response["error"] = $error;
+	}
+
+	return json_encode($response);
+}
+
 ?>
