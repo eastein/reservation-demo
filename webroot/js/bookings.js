ce33b808b69a464703f3ce54e42309ae6b30cf2f
Zach Reed <zreed@bluetideproductions.com>
2014-02-06 14:46:15 -0600

    login test

---
+++ webroot/js/bookings.js
@@ -0,0 +1,35 @@
+$("#signIn").on("submit", function(event) {
+	event.preventDefault();
+	var loginData = {
+	    password: $(this).find("#signInPass").val() //hunter2
+	};
+	console.log(loginData);
+	$.ajax({
+		url : "/api/login",
+		type: "POST",
+		data: loginData,
+		success: function(data, textStatus, jqXHR){
+			console.log(data);
+		},
+		error: function (jqXHR, textStatus, errorThrown) {
+			console.log(textStatus);
+		}
+	});
+});
+function confirmDelete() {
+	var x;
+	var r=confirm("Press a button!");
+	if (r==true) {
+		x="You pressed OK!";
+	} else {
+		x="You pressed Cancel!";
+	}
+	document.getElementById("demo").innerHTML=x;
+}
+$(".form_datetime").datetimepicker({
+	format: "MM dd, yyyy - HH:ii P",
+	autoclose: true,
+	pickerPosition: "bottom-left",
+	todayBtn: false,
+	showMeridian: true
+});
