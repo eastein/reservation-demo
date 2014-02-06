cdfef0131a04f361516a8177e902a3277a14f4b1
joe kaiser <joe.kaiser@parkwhiz.com>
2014-02-06 14:34:33 -0600

    require_once

--- backend/backend.php
+++ backend/backend.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once('mysqlidb.php');
+require_once('db_obj.php');
 
 $adminpw = "hunter2";
 
@@ -29,41 +29,3 @@ function handle_api() {
 
 	return json_encode($response);
 }
-
-$db = new Mysqlidb('localhost', 'booking', 'booking', 'booking');
-
-//insert
-
-/*$insertData = array(
-	'start' => time(),
-	'end' => time(),
-	'name' => "John Doe"
-);
-
-if($db->insert('booking', $insertData)) echo 'success!';*/
-
-//select
-
-/*$results = $db->get('booking', 100);
-print_r($results);*/
-
-//select from time range
-
-/*
-$db->where('start', time());
-$db->where('end', time());
-$results = $db->get('booking');
-print_r($results);*/
-
-//update
-/*$updateData = array(
-	'start' => time(),
-	'end' => time(),
-	'name' => "John Doe"
-);
-$db->where('id', 1);
-$results = $db->update('booking', $updateData);*/
-
-//delete
-/*$db->where('id', 1);
-if($db->delete('booking')) echo 'successfully deleted'; */
