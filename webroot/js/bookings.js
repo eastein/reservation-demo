a2b5e42470cc9a45629b01f6a6b8ecaf2cffb305
Zach Reed <zreed@bluetideproductions.com>
2014-02-06 15:00:55 -0600

    admin login

--- webroot/js/bookings.js
+++ webroot/js/bookings.js
@@ -12,7 +12,7 @@ $("#signIn").on("submit", function(event) {
 		success: function(data, textStatus, jqXHR){
 			console.log(data);
 			if(!data["error"]){
-				window.location = "/login";
+				window.location = "/admin";
 			} else {
 				console.log('error');
 				$this.find('form-group').addClass("has-error has-feedback").append('<span class="glyphicon glyphicon-remove form-control-feedback"></span>');
